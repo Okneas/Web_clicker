889416940a656ed619b50df4de07809d97984d87
Okneas <hantulin2@mail.ru>
2023-10-16 22:54:29 +0300

    добавил реализацию построек для пассивного дохода в секунду

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import { thisPlayer } from './index';
+import { Building } from './building';
 
 function App() {
   const [clicks, setClicks] = useState(thisPlayer.pointsInTotal);
@@ -19,14 +20,27 @@ function App() {
     };
   }, []);
 
-  return (
-    <div>
-      <div className="clicker" onClick={AddOnClick}></div>
-      <div className='info_clicks'>
-        <p>Кол-во очков {clicks}</p>
-      </div>
-      <div>
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setClicks(thisPlayer.pointsInTotal);
+    }, 1);
 
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <div className='main-block'>
+      <div className='clicker-block'>
+        <div className="clicker" onClick={AddOnClick}>
+        </div>
+        <div className='info_clicks'>
+          <p>Кол-во очков {clicks.toFixed(2)}</p>
+        </div>
+      </div>
+      <div className='building-panel'>
+        <Building name='Bulding 1' count={0} baseCost={25} profitPerSecond={1} price={25}/>
       </div>
     </div>
   );
--- my-app/src/building.tsx
+++ my-app/src/building.tsx
@@ -1,22 +1,56 @@
+import { useState, useRef } from "react";
+import { thisPlayer } from "./index";
+
 class BuildingClass {
     name: string;
     count: number;
+    baseCost: number;
     price: number;
     profitPerSecond: number;
-    constructor(name: string, price: number, count: number, PPS: number){
+    constructor(name: string, baseCost: number, count: number, PPS: number){
+        this.price = baseCost;
         this.name = name;
-        this.price = price;
+        this.baseCost = baseCost;
         this.count = count;
         this.profitPerSecond = PPS;
     }
 }
 
+interface BuildingProps {
+    name: string;
+    count: number;
+    baseCost:number;
+    price: number;
+    profitPerSecond: number;
+}
 
-export function Building() {
-    
+export function Building(props: BuildingProps) {
+    let buildingEntity = useRef(new BuildingClass(props.name, props.baseCost, props.count, props.profitPerSecond));
+    let [stats, setStats] = useState({
+        name: buildingEntity.current.name,
+        price: buildingEntity.current.price,
+        count: buildingEntity.current.count,
+        PPS: buildingEntity.current.profitPerSecond,
+    });
+
+    let onBuy = () => {
+        if(thisPlayer.pointsInTotal >= buildingEntity.current.price) {
+            thisPlayer.pointsInTotal -= buildingEntity.current.price;
+            thisPlayer.pointsPerSecond+=buildingEntity.current.profitPerSecond;
+            buildingEntity.current.count++;
+            buildingEntity.current.price = Math.round(buildingEntity.current.baseCost*(1.15**buildingEntity.current.count)*100)/100;
+            console.log(buildingEntity.current.count);
+            console.log(buildingEntity.current.price);
+            setStats({name: buildingEntity.current.name,
+                price: buildingEntity.current.price,
+                count: buildingEntity.current.count,
+                PPS: buildingEntity.current.profitPerSecond});
+        }
+    }
     return (
-        <div>
-
+        <div className="building-block" onClick={onBuy}>
+            <p>Name: {stats.name} Price: {stats.price}</p>
+            <p>Count: {stats.count} Profit Per Second: {stats.PPS}</p>
         </div>
     );
 }
